476e0f8a46c6d01c1971ccbb947731d388648223
Rajesh171088 <rajesh171088@gmail.com>
2023-09-01 09:09:26 +0530

    Webdrivermanager updated

--- src/test/java/prac/PrintTest.java
+++ src/test/java/prac/PrintTest.java
@@ -6,13 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class PrintTest {
 	
 	@Test
 	public void trialTest() throws InterruptedException
 	{
 		String browser="chrome";
-		
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		String url="https://mail.google.com/mail/u/0/#inbox";
 		driver.get(url);
