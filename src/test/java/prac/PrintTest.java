c5cad104d6f463e90afd57a936cf1d33c37d3352
Rajesh171088 <rajesh171088@gmail.com>
2023-09-01 09:40:02 +0530

    push

--- src/test/java/prac/PrintTest.java
+++ src/test/java/prac/PrintTest.java
@@ -4,18 +4,34 @@ import java.sql.DriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PrintTest {
-	
+	@Parameters("browser")
 	@Test
-	public void trialTest() throws InterruptedException
+	public void trialTest(String browser) throws InterruptedException
 	{
-		String browser="chrome";
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		System.getProperty("browser","chrome");
+		WebDriver driver;
+		if(browser.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("Edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}
+		else
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
 		String url="https://mail.google.com/mail/u/0/#inbox";
 		driver.get(url);
 		Thread.sleep(2000);
